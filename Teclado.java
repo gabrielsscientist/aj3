f3c504a672b083bd94c367b7efbd6a009da3ebd2
gabrielsscientist <53708782+gabrielsscientist@users.noreply.github.com>
2020-10-02 11:51:11 -0300

    lab03_03

---
+++ Teclado.java
@@ -0,0 +1,11 @@
+package br.com.globalcode.aj3.util;
+
+import java.util.Scanner;
+
+public class Teclado {
+
+	public static String le() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextLine();
+	}
+}
